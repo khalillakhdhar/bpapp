44f9ebf6dc1a216aa8b3e1c8c2156474bda5257a
khalil <khalillakhdharatc@gmail.com>
2023-06-08 14:51:11 +0100

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bpapp';
+  rappel={tache:'',date:''};
+  rappels:any=[];
+  ngOnInit(): void {
+
+    this.rappels=JSON.parse(localStorage.getItem("rappels")||"");
+
+  }
+
+
+
+  addRappel()
+  {
+    this.rappels.push(this.rappel);
+    // convertir en chaine et stocker dans localStorage
+    localStorage.setItem("rappels",JSON.stringify(this.rappels));
+
+    this.rappel={tache:'',date:''};
+
+
+  }
+  deleteRappel(index:number)
+  {
+    if(confirm('Are you sure you want to delete'))
+    {
+      this.rappels.splice(index, 1);
+      localStorage.setItem("rappels",JSON.stringify(this.rappels));
+
+    }
+
+
+  }
 }
